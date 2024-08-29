0b97b895588eee8628c59aad6cf073e750a0fe63
juniorgeyer <junior_geyer@hotmail.com>
2024-08-29 10:39:14 -0300

    correcao header e footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="container flex flex-col items-center px-10 pb-8 mx-auto border-t border-purple-600 dark:border-purple-300 sm:flex-row">
         <Link href="/">
           <a className="flex items-center justify-center mt-3 text-xl font-medium title-font md:ml-3 md:justify-start">
-            MS MEGA SOLAR
+            CTECH
           </a>
         </Link>
         <div className="invisible h-5 mt-3 ml-4 border-l border-black md:visible dark:border-gray-300"></div>
